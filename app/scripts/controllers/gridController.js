b08b3896ffd5d019b0b1cd9546870239377a35ed
Ricky Chien <ricky060709@gmail.com>
2014-06-19 23:30:07 +0800

    Add itemDetail service

--- app/scripts/controllers/gridController.js
+++ app/scripts/controllers/gridController.js
@@ -1,15 +1,37 @@
 define(['app', 'services/service'], function(app) {
   'use strict';
 
-  app.controller('GridController', ['$scope', '$routeParams', 'Service',
-    function($scope, $routeParams, Service) {
-      $scope.items = Service.get({
-        type: $routeParams.type
+  app.controller('GridController', [
+    '$scope',
+    '$routeParams',
+    'Item',
+    'ItemDetail',
+    function($scope, $routeParams, Item, ItemDetail) {
+      var type = $routeParams.type;
+
+      $scope.items = Item.get({
+        type: type
       });
+
+      $scope.itemDetail = {};
 
       $scope.getRate = function(item) {
         return (item.readCount / ((item.readCount + item.unreadCount) || 1) *
           100).toFixed(2);
+      };
+
+      $scope.queryItemDetail = function(item) {
+        var itemId = item.id,
+            itemDetail = $scope.itemDetail;
+
+        if (itemDetail[itemId]) {
+          return;
+        }
+
+        itemDetail[itemId] = ItemDetail.get({
+          type: type,
+          id: itemId
+        });
       };
     }
   ]);
--- app/scripts/services/service.js
+++ app/scripts/services/service.js
@@ -1,8 +1,14 @@
 define(['app'], function(app) {
   'use strict';
 
-  app.factory('Service', ['$resource', function($resource) {
+  app.factory('Item', ['$resource', function($resource) {
     return $resource('api/:type.json', {}, {
+      get: { method: 'GET', isArray: true }
+    });
+  }]);
+
+  app.factory('ItemDetail', ['$resource', function($resource) {
+    return $resource('api/:type/:id.json', {}, {
       get: { method: 'GET', isArray: true }
     });
   }]);
